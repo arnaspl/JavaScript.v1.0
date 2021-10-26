3c7cc3cab760741a00c3fae926841e3420aec78d
arnaspl <60697454+arnaspl@users.noreply.github.com>
2021-10-26 14:59:20 +0300

    Added .js file

---
+++ index.js
@@ -0,0 +1,74 @@
+// var firstName = "Arnas";
+// var age = 22;
+// var isMale = true;
+// var lastNameLetter = 'P';
+// var selectedCities = [];
+// console.log(lastNameLetter);
+
+// let person = {
+//     firstName: 'Edvardas',
+//     age: 30,
+//     isMale: true,
+//     city: {
+//         address: 'Gyneju 4'
+//     }
+// };
+
+// // Dot notation
+// person.firstName= 'Jonas';
+
+// // Bracket notation
+
+// person['firstName'] = 'Tomas';
+
+// console.log(person);
+
+// const selectedCities = ['Vilnius', 'Kaunas'];
+
+// console.log(selectedCities);
+
+// const objectArray = [
+//     {
+//         id: 1,
+//         name: 'Arnas',
+//         jobTitle: 'POS Support'
+//     }
+// ]
+
+// Task1
+
+var person = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    hobbies: ["Art", "Programming", "Games"],
+    address: {
+      street: "Gostauto",
+      city: "Vilnius",
+    },
+  },
+];
+
+var objectArray = [1, "red", "blue", false, { name: "John" }];
+
+var tasks = [
+  {
+    id: 1,
+    text: "Task 1",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: "Task 2",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "Task 4",
+    isCompleted: true,
+  },
+];
+
+console.log(person);
+console.log(objectArray);
+console.log(tasks);
